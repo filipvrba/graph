de0b561efeb76ad64cd18a46ac0fbb4949c1adfa
Filip Vrba <filipvrba@pm.me>
2021-08-22 12:12:47 +0200

    remove core on root folder

--- core/engine.js
+++
@@ -1,8 +0,0 @@
-export { Vector2 } from './math/vector2.js';
-export { Mathf } from './math/mathf.js';
-export { Clock } from './objects/clock.js';
-export { PieChart } from './objects/graphs/piechart.js';
-export { Color } from './math/color.js';
-export { Object2D } from './objects/object2d.js';
-export { Scene } from './objects/scene.js';
-export { Transform2D } from './math/transform2d.js'
