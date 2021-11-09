1096a245e3fb453b0288c4dd85b00afbd1b91f4c
Filip Vrba <filipvrba@pm.me>
2021-11-09 02:24:03 +0100

    detect touchscreen for filter element

--- docs/elements/menu/filterElement.js
+++ docs/elements/menu/filterElement.js
@@ -90,14 +90,38 @@ class FilterElement extends HTMLElement {
 
     }
 
-    clickHome( isVisibleMenu ) {
+    hasTouchScreen() {
 
-        if ( isVisibleMenu ) {
-
-            this.filterInput.focus();
-
+        let hasTouchScreen;
+        if ("maxTouchPoints" in navigator) {
+            hasTouchScreen = navigator.maxTouchPoints > 0;
+        } else if ("msMaxTouchPoints" in navigator) {
+            hasTouchScreen = navigator.msMaxTouchPoints > 0;
+        } else {
+            var mQ = window.matchMedia && matchMedia("(pointer:coarse)");
+            if (mQ && mQ.media === "(pointer:coarse)") {
+                hasTouchScreen = !!mQ.matches;
+            } else if ('orientation' in window) {
+                hasTouchScreen = true; // deprecated, but good fallback
+            } else {
+                // Only as a last resort, fall back to user agent sniffing
+                var UA = navigator.userAgent;
+                hasTouchScreen = (
+                    /\b(BlackBerry|webOS|iPhone|IEMobile)\b/i.test(UA) ||
+                    /\b(Android|Windows Phone|iPad|iPod)\b/i.test(UA)
+                );
+            }
         }
 
+        return hasTouchScreen;
+    }
+
+    clickHome( isVisibleMenu ) {
+
+        if ( isVisibleMenu && !this.hasTouchScreen() ) {
+
+            this.filterInput.focus();
+        }
     }
 
     connectedCallback() {
